536b392ebed15837c2bf47a19979aac288278ea0
K.Kiyokawa <koichi20110068@gmail.com>
2021-03-03 21:55:32 +0900

    chore: setup

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import { AppModule } from './app.module'
+import * as express from 'express'
+import * as functions from 'firebase-functions'
+import * as helmet from 'helmet'
+
+const server = express()
+
+const createNestServer = async (expressInstance) => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance))
+
+  app.use(helmet())
+  app.enableCors()
+
+  console.log('the server is starting @ firebase')
+  return app.init()
+}
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err))
+
+export const api = functions.https.onRequest(server)
